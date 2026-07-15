9f92e82c6cb4f05ebf208834a62e749e67b0a95b
mplachk <markplachkov@gmail.com>
2026-07-15 15:33:43 -0400

    Enhance Terrain class with flood map functionality
    Added constructor and flood map printing method description.

--- oop/Terrain/Terrain.java
+++ oop/Terrain/Terrain.java
@@ -1,4 +1,22 @@
 /**
+ * You are given a two-dimensional array of values that give the height of a terrain at different points in a square. Write a constructor public Terrain(double[][] heights) and a method public void printFloodMap(double waterLevel) that prints out a flood map, showing which of the points in the terrain would be flooded if the water level was the given value. In the flood map, print a * for each flooded point and a space for each point that is not flooded.
+ * Here is a sample map:
+  * * * * 	        	* *
+  * * * * *           * * *
+  * * * *	            * *
+  * * *	              * * *
+  * * * *         *   * * *
+  * * * * * * * * * *
+  * *      * * *
+  *        * * * *        *
+                        * *
+                      * * *
+
+ * Then write a program that reads one hundred terrain height values and shows how the terrain gets flooded when the water level increases in ten steps from the lowest point in the terrain to the highest.
+ */
+
+
+/**
  * Represents a terrain as a grid of height values and can print
  * a flood map showing which points would be underwater at a
  * given water level.
